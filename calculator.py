d799497d7cd289f040b66bcde376e5deb48f8857
cyber-moni <m0204aryam@gmail.com>
2026-08-11 21:09:38 +0500

    Implement basic calculator operations
    Added support for multiple arithmetic operations based on user input.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,12 @@
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
-result = a + b
+operation = input(" Введите репарацию (+,-,*,/):")
+if operation == "+":
+  result = a + b
+elif operation == "-":
+  result = a - b
+elif operation == "*":
+  result = a * b
+elif operation == "/":
+  result = a/ b
 print("Ответ:" ,result)
