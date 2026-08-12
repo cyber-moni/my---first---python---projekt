e922c7ab4c9e61311b6686362d175c87c8853995
cyber-moni <m0204aryam@gmail.com>
2026-08-12 18:17:13 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,4 +9,8 @@ elif operation == "*":
   result = a * b
 elif operation == "/":
   result = a/ b
+else:
+  result = None
+  print("Неизвестное действие")
+  if result is not None
 print("Ответ:" ,result)
