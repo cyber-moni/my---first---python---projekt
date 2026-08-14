e582a9d83f40e7d8046416a84d47fce7a70193e3
cyber-moni <m0204aryam@gmail.com>
2026-08-14 22:28:03 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,35 @@
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
-operation = input(" Введите репарацию (+,-,*,/):")
-if operation == "+":
-  result = a + b
-elif operation == "-":
-  result = a - b
-elif operation == "*":
-  result = a * b
-elif operation == "/":
-  result = a/ b
-else:
-  print("Неизвестное действие")
-  result = None
-  if result is not None
-print("Ответ:" ,result)
+answer = "да"
 
+while answer == "да":
+  a = int(input("Введите первое число:"))
+  b = int(input("Введите второе число:"))
+  operation = input("Введите операцию (+, -, *, /, %):")
+  if operation == "+":
+    result = a + b
+  elif operation == "-":
+    result = a - b
+  elif operation == "*":
+    result = a * b
+  elif operation == "/":
+    if b == 0:
+      print(" Не делится на ноль!")
+      result = None
+    else:
+      result = a / b
+  elif operation == "%":
+    result = a % b
+  else:
+    print("Неизвестное действие!")
+    result = None
+  if result is not None:
+    print("Ответ:" , result)
+  answer = input("Хотите посчитать ещё? да/нет?").lower().strip()
+  while answer !="да" and answer !="нет":
+    answer = input("Пожалуйста, введите да или нет: ").lower().strip()
+  
+ print ("До свидания !")
+    
+  
 
 
 
